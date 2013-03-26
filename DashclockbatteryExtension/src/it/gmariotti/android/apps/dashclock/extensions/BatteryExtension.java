895585e4de242b209e55f6b204702a5c6ead5e21
gabrielemariotti <gabri.mariotti@gmail.com>
2013-03-26 23:23:04 +0100

    Add receiver for battery changes

--- DashclockbatteryExtension/src/it/gmariotti/android/apps/dashclock/extensions/BatteryExtension.java
+++ DashclockbatteryExtension/src/it/gmariotti/android/apps/dashclock/extensions/BatteryExtension.java
@@ -23,9 +23,15 @@ import android.content.SharedPreferences;
 import android.content.pm.ResolveInfo;
 import android.os.BatteryManager;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
+
+import static it.gmariotti.android.apps.dashclock.extensions.LogUtils.LOGD;
+import static it.gmariotti.android.apps.dashclock.extensions.LogUtils.LOGI;
+import static it.gmariotti.android.apps.dashclock.extensions.LogUtils.LOGE;
+
 
 public class BatteryExtension extends DashClockExtension {
 
@@ -35,11 +41,18 @@ public class BatteryExtension extends DashClockExtension {
 	public static final String PREF_BATTERY_CHARGE = "pref_battery_charge";
 	public static final String PREF_BATTERY_VOLTAGE = "pref_battery_voltage";
 	public static final String PREF_BATTERY_TEMP = "pref_battery_temp";
-
+	public static final String PREF_BATTERY_HEALTH = "pref_battery_health";
+	public static final String PREF_BATTERY_REALTIME = "pref_battery_realtime";
+	
+	//Receiver for ACTION_BATTERY_CHANGED
+	private BatteryChangeReceiver mBatteryChangeReceiver=null;  
+	
 	// Prefs
 	protected boolean prefCharge = true;
 	protected boolean prefTemp = true;
 	protected boolean prefVoltage = true;
+	protected boolean prefHealth = true;
+	protected boolean prefRealtime = false;
 
 	// Value
 	private int level;
@@ -49,11 +62,14 @@ public class BatteryExtension extends DashClockExtension {
 	private int temperature;
 	private String umTemp;
 	private String umVoltage = "";
+	private String health;
 
 	@Override
 	protected void onInitialize(boolean isReconnect) {
 		super.onInitialize(isReconnect); 
 		if (!isReconnect) {
+			readPreferences();
+			
 			//Listener for change battery: I prefer listen only for power change. 
 			//You can change it with ACTION_BATTERY_CHANGED
 			IntentFilter filter=new IntentFilter();
@@ -62,14 +78,21 @@ public class BatteryExtension extends DashClockExtension {
 			
 			getApplicationContext().registerReceiver(mBatteryReceiver,
 					filter);
+			
+			//User can choose for ACTION_BATTERY_CHANGED
+			registerChangeReceiver();
 		}
 	}
 
 	@Override
 	protected void onUpdateData(int reason) {
+		LOGD(TAG, "onUpdate "+reason);
 		// Read Preferences
 		readPreferences();
 
+		//Register and Unregister changeReceiver
+		registerChangeReceiver();
+		
 		// readBatteryData
 		readBatteryData(null);
 
@@ -80,6 +103,7 @@ public class BatteryExtension extends DashClockExtension {
 		// publish
 		publishUpdateExtensionData();
 	}
+	
 
 	private void readBatteryData(Intent batteryStatus) {
 
@@ -92,10 +116,14 @@ public class BatteryExtension extends DashClockExtension {
 
 		// Level
 		level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-
-		// health
-		// int health = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH,
-		// -1);
+		LOGD(TAG,"level="+level);
+		
+		 // health
+		int healthState = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+		int healthId = getResources().getIdentifier("battery_health_" + healthState,
+					"string", this.getPackageName());
+		health = getString(healthId);
+		LOGD(TAG,"healthState="+healthState); 
 
 		// How are we charging?
 		int chargePlug = batteryStatus.getIntExtra(
@@ -103,7 +131,8 @@ public class BatteryExtension extends DashClockExtension {
 		boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
 		boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
 		boolean wirelessCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS;
-
+		
+		
 		charge = null;
 		if (usbCharge || acCharge || wirelessCharge) {
 			int resId = getResources().getIdentifier("charge_" + chargePlug,
@@ -124,6 +153,7 @@ public class BatteryExtension extends DashClockExtension {
 		// boolean isFull = status == BatteryManager.BATTERY_STATUS_FULL;
 		// boolean isDischarging = status ==
 		// BatteryManager.BATTERY_STATUS_DISCHARGING;
+		LOGD(TAG, "status="+status);
 
 		int resIdCharging = getResources().getIdentifier("charging_" + status,
 				"string", this.getPackageName());
@@ -141,7 +171,8 @@ public class BatteryExtension extends DashClockExtension {
 		temperature = batteryStatus.getIntExtra(
 				BatteryManager.EXTRA_TEMPERATURE, 0);
 		voltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
-
+		LOGD(TAG,"temperature="+temperature);
+		LOGD(TAG,"voltage="+voltage);
 	}
 
 	/**
@@ -174,6 +205,12 @@ public class BatteryExtension extends DashClockExtension {
 			sb.append(umVoltage);
 			and = " - ";
 		}
+		
+		if (prefHealth) {
+			sb.append(and);
+			sb.append(health);
+			and = " - ";
+		}
 
 		// Publish the extension data update.
 		publishUpdate(new ExtensionData().visible(true)
@@ -192,8 +229,32 @@ public class BatteryExtension extends DashClockExtension {
 		prefVoltage = sp.getBoolean(PREF_BATTERY_VOLTAGE, true);
 		prefCharge = sp.getBoolean(PREF_BATTERY_CHARGE, true);
 		prefTemp = sp.getBoolean(PREF_BATTERY_TEMP, true);
+		prefHealth = sp.getBoolean(PREF_BATTERY_HEALTH, true);
+		prefRealtime = sp.getBoolean(PREF_BATTERY_REALTIME, false);
 	}
 
+	
+	private void registerChangeReceiver(){
+		//User can choose for ACTION_BATTERY_CHANGED
+		LOGD(TAG,"real time="+prefRealtime);
+		if (prefRealtime){
+			//Register receiver if not already registered
+			 if (mBatteryChangeReceiver==null){
+				 IntentFilter ifilter = new IntentFilter(
+						 Intent.ACTION_BATTERY_CHANGED);
+				 mBatteryChangeReceiver=new BatteryChangeReceiver();
+				 getApplicationContext().registerReceiver(mBatteryChangeReceiver,
+						 ifilter);
+			 }
+		}else{
+			//I don't want real time
+			//If receiver has already register, unregister it
+			if (mBatteryChangeReceiver!=null){
+				getApplicationContext().unregisterReceiver(mBatteryChangeReceiver);
+				mBatteryChangeReceiver=null;
+			}
+		}
+	}
 	
 	private BroadcastReceiver mBatteryReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
@@ -201,4 +262,12 @@ public class BatteryExtension extends DashClockExtension {
 		}
 	};
 
+	
+	private class BatteryChangeReceiver extends BroadcastReceiver {
+		
+		public void onReceive(Context context, Intent intent) {
+			LOGD(TAG,"BatteryChangeReceiver");
+			onUpdateData(UPDATE_REASON_CONTENT_CHANGED);
+		}
+	}
 }
